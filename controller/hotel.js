cac36b14c6cea205bae9cd8aa07d6e31d1469316
userselu <52342206+SilvinPradhan@users.noreply.github.com>
2021-09-22 14:42:09 -0500

    implement endpoint to get all the seller hotels by filtering postedBy id on the database

--- controller/hotel.js
+++ controller/hotel.js
@@ -2,48 +2,56 @@ const Hotel = require("../models/hotel");
 const fs = require("fs");
 
 exports.create = async (req, res) => {
-  //   console.log("request fields", req.fields);
-  try {
-    let fields = req.fields;
-    let files = req.files;
+    //   console.log("request fields", req.fields);
+    try {
+        let fields = req.fields;
+        let files = req.files;
 
-    let hotel = new Hotel(fields);
-    hotel.postedBy = req.user._id;
-    // handle image - read the entire file before saving to the db (asynchronously)
-    if (files.image) {
-      hotel.image.data = fs.readFileSync(files.image.path);
-      hotel.image.contentType = files.image.type;
+        let hotel = new Hotel(fields);
+        hotel.postedBy = req.user._id;
+        // handle image - read the entire file before saving to the db (asynchronously)
+        if (files.image) {
+            hotel.image.data = fs.readFileSync(files.image.path);
+            hotel.image.contentType = files.image.type;
+        }
+
+        hotel.save((err, result) => {
+            if (err) {
+                console.log("saving hotel err", err);
+                res.status(400).send("Error saving hotel in the db");
+            }
+            res.json(result);
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(400).json({
+            err: err.message,
+        });
     }
-
-    hotel.save((err, result) => {
-      if (err) {
-        console.log("saving hotel err", err);
-        res.status(400).send("Error saving hotel in the db");
-      }
-      res.json(result);
-    });
-  } catch (err) {
-    console.log(err);
-    res.status(400).json({
-      err: err.message,
-    });
-  }
 };
 
 exports.hotels = async (req, res) => {
-  let all = await Hotel.find({})
-    .limit(12)
-    .select("-image.data")
-    .populate("postedBy", "_id name")
-    .exec();
-  console.log(all);
-  res.json(all);
+    let all = await Hotel.find({})
+        .limit(12)
+        .select("-image.data")
+        .populate("postedBy", "_id name")
+        .exec();
+    console.log(all);
+    res.json(all);
 };
 
 exports.image = async (req, res) => {
-  let hotel = await Hotel.findById(req.params.hotelId).exec();
-  if (hotel && hotel.image && hotel.image.data !== null) {
-    res.set("Content-Type", hotel.image.contentType);
-    return res.send(hotel.image.data);
-  }
+    let hotel = await Hotel.findById(req.params.hotelId).exec();
+    if (hotel && hotel.image && hotel.image.data !== null) {
+        res.set("Content-Type", hotel.image.contentType);
+        return res.send(hotel.image.data);
+    }
 };
+
+exports.sellerHotels = async (req, res) => {
+    let all = await Hotel.find({postedBy: req.user._id})
+        .select('-image.data')
+        .populate('postedBy', '_id name')
+        .exec()
+    res.send(all)
+}
