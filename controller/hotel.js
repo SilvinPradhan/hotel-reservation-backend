c9683890987002ffa4bcb4b662e9441fd4d20543
SilvinPradhan <silvin.pradhan-2@selu.edu>
2021-09-26 18:59:22 -0500

    implementation of updating single hotel with id without image data

--- controller/hotel.js
+++ controller/hotel.js
@@ -1,71 +1,103 @@
 const Hotel = require("../models/hotel");
 const fs = require("fs");
-const {remove} = require("./hotel");
+const { remove } = require("./hotel");
 
 exports.create = async (req, res) => {
-    //   console.log("request fields", req.fields);
-    try {
-        let fields = req.fields;
-        let files = req.files;
+  //   console.log("request fields", req.fields);
+  try {
+    let fields = req.fields;
+    let files = req.files;
 
-        let hotel = new Hotel(fields);
-        hotel.postedBy = req.user._id;
-        // handle image - read the entire file before saving to the db (asynchronously)
-        if (files.image) {
-            hotel.image.data = fs.readFileSync(files.image.path);
-            hotel.image.contentType = files.image.type;
-        }
+    console.log(fields);
+    console.log(files);
 
-        hotel.save((err, result) => {
-            if (err) {
-                console.log("Saving Hotel Error", err);
-                res.status(400).send("Error saving hotel data in the database.");
-            }
-            res.json(result);
-        });
-    } catch (err) {
-        console.log(err);
-        res.status(400).json({
-            err: err.message,
-        });
+    let hotel = new Hotel(fields);
+    hotel.postedBy = req.user._id;
+    // handle image - read the entire file before saving to the db (asynchronously)
+    if (files.image) {
+      hotel.image.data = fs.readFileSync(files.image.path);
+      hotel.image.contentType = files.image.type;
     }
+
+    hotel.save((err, result) => {
+      if (err) {
+        console.log("Saving Hotel Error", err);
+        res.status(400).send("Error saving hotel data in the database.");
+      }
+      res.json(result);
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({
+      err: err.message,
+    });
+  }
 };
 
 exports.read = async (req, res) => {
-    let hotel = await Hotel.findById(req.params.hotelId).select('-image.data').exec()
-    console.log("Single Hotel Information Page", hotel);
-    res.json(hotel)
-}
+  let hotel = await Hotel.findById(req.params.hotelId)
+    .select("-image.data")
+    .exec();
+  console.log("Single Hotel Information Page", hotel);
+  res.json(hotel);
+};
 
 exports.hotels = async (req, res) => {
-    let all = await Hotel.find({})
-        .limit(12)
-        .select("-image.data")
-        .populate("postedBy", "_id name")
-        .exec();
-    console.log(all);
-    res.json(all);
+  let all = await Hotel.find({})
+    .limit(12)
+    .select("-image.data")
+    .populate("postedBy", "_id name")
+    .exec();
+  console.log(all);
+  res.json(all);
 };
 
 exports.image = async (req, res) => {
-    let hotel = await Hotel.findById(req.params.hotelId).exec();
-    if (hotel && hotel.image && hotel.image.data !== null) {
-        res.set("Content-Type", hotel.image.contentType);
-        return res.send(hotel.image.data);
-    }
+  let hotel = await Hotel.findById(req.params.hotelId).exec();
+  if (hotel && hotel.image && hotel.image.data !== null) {
+    res.set("Content-Type", hotel.image.contentType);
+    return res.send(hotel.image.data);
+  }
 };
 
 exports.sellerHotels = async (req, res) => {
-    let all = await Hotel.find({postedBy: req.user._id})
-        .select('-image.data')
-        .populate('postedBy', '_id name')
-        .exec()
-    res.send(all)
-}
+  let all = await Hotel.find({ postedBy: req.user._id })
+    .select("-image.data")
+    .populate("postedBy", "_id name")
+    .exec();
+  res.send(all);
+};
 
 exports.remove = async (req, res) => {
-    let deleted = await Hotel.findByIdAndDelete(req.params.hotelId)
-        .select("-image.data")
-        .exec()
-    res.json(deleted)
-}
+  let deleted = await Hotel.findByIdAndDelete(req.params.hotelId)
+    .select("-image.data")
+    .exec();
+  res.json(deleted);
+};
+
+exports.update = async (req, res) => {
+  try {
+    let fields = req.fields;
+    let files = req.files;
+    let data = { ...fields };
+
+    if (files.image) {
+      let image = {};
+      image.data = fs.readFileSync(files.image.path);
+      image.contentType = files.image.type;
+
+      data.image = image;
+      console.log(image);
+    }
+
+    let updated = await Hotel.findByIdAndUpdate(req.params.hotelId, data, {
+      new: true,
+    }).select("-image.data");
+
+    console.log(updated);
+    res.json(updated);
+  } catch (error) {
+    console.log(error);
+    res.status(400).send("Hotel update failed. try again.");
+  }
+};
--- routes/hotel.js
+++ routes/hotel.js
@@ -1,14 +1,29 @@
 const express = require("express");
 const formidable = require("express-formidable");
 const router = express.Router();
-const {create, read, hotels, image, sellerHotels, remove} = require("../controller/hotel");
-const {requireSignin, hotelOwner} = require("../middlewares/index");
+const {
+  create,
+  read,
+  hotels,
+  image,
+  update,
+  sellerHotels,
+  remove,
+} = require("../controller/hotel");
+const { requireSignin, hotelOwner } = require("../middlewares/index");
 
 router.post("/create-hotel", requireSignin, formidable(), create);
 router.get("/hotels", hotels);
-router.get("/hotel/image/:hotelId", image)
-router.delete("/delete-hotel/:hotelId", requireSignin, hotelOwner, remove)
-router.get("/hotel/:hotelId", read)
+router.get("/hotel/image/:hotelId", image);
+router.delete("/delete-hotel/:hotelId", requireSignin, hotelOwner, remove);
+router.get("/hotel/:hotelId", read);
+router.put(
+  "/update-hotel/:hotelId",
+  requireSignin,
+  hotelOwner,
+  formidable(),
+  update
+);
 router.get("/seller-hotels/", requireSignin, sellerHotels);
 
 module.exports = router;
